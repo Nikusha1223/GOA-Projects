95cda5b12ae4a9aa5436549a0374811af05c750b
Nikusha1233 <lomidze372@gmail.com>
2025-05-12 20:57:53 +0400

    second commit

---
+++ Day 26/ClassWork/task2.py
@@ -0,0 +1,19 @@
+# 
+
+full_name = "nika lomidze"  
+result = ""
+
+for name in full_name: 
+    print(full_name)  
+    if name == "n":
+        result += "N" 
+    elif name == "i": 
+        result += "I"
+    elif name == "k":
+        result += "K"
+    else :
+        result += " " 
+
+print(result)
+
+
